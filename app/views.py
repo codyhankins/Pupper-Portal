3574d24795a77615c3ca84075cc9f761f6ffd103
Cody Hankins <chankinsoft@gmail.com>
2019-03-10 17:35:53 -0700

    got the redirect to show up for the results page

--- app/views.py
+++ app/views.py
@@ -1,12 +1,20 @@
 from flask import render_template
-
+from flask import request
 from app import app
 
+
+ButtonPressed = 0        
+@app.route('/button', methods=["GET", "POST"])
+def button():
+    if request.method == "POST":
+        # need to get the info from the form and process it here
+        return render_template("results.html", ButtonPressed = ButtonPressed)
+        # I think you want to increment, that case ButtonPressed will be plus 1.
+    return render_template("button.html", ButtonPressed = ButtonPressed)
 
 @app.route('/')
 def index():
     return render_template("index.html")
-
 
 @app.route('/about')
 def about():
